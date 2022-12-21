b7aa8eccee4b1cb556e6c52d30175a93c176f76f
Écio Ferraz <ecioferraz@hotmail.com>
2022-12-21 20:05:19 -0300

    feat: axios makeRequest

---
+++ src/api/axios.ts
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = 'http://localhost:3000/';
+
+type AxiosProps = {
+  endpoint: string;
+  method: 'get' | 'post' | 'put' | 'patch' | 'delete';
+  body?: object;
+  id?: string;
+  token?: string;
+};
+
+export default async function makeRequest({
+  endpoint,
+  method,
+  body,
+  id,
+  token,
+}: AxiosProps) {
+  return axios(endpoint, {
+    data: body,
+    headers: { Authorization: `Bearer ${token}` },
+    method,
+    params: id || body,
+  });
+}
