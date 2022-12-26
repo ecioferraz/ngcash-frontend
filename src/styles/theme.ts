e2cc8fe9533fc39bf13853df5673273267f81747
Écio Ferraz <ecioferraz@hotmail.com>
2022-12-26 15:31:23 -0300

    fix: theme xsmall size

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -11,7 +11,7 @@ export const theme = {
       secondary: "'Montserrat', sans-serif",
     },
     sizes: {
-      xsmall: '8rem',
+      xsmall: '0.8rem',
       small: '1.6rem',
       medium: '2.4rem',
       large: '3.2rem',
