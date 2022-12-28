db4554e0b380882ea4f22a103f6f24a978b95a22
Écio Ferraz <ecioferraz@hotmail.com>
2022-12-28 17:09:56 -0300

    feat: Login page redirect

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,5 +1,12 @@
+import Router from 'next/router';
+import { useEffect } from 'react';
+import { readUser } from '../services/localStorage';
 import LoginForm from '../templates/LoginForm';
 
 export default function Login() {
+  useEffect(() => {
+    readUser() ? Router.push('/account') : Router.push('/login');
+  }, []);
+
   return <LoginForm />;
 }
