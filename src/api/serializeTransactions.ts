5fdf340a976347c5c1afcb2169fb9522182cc1c7
Écio Ferraz <ecioferraz@hotmail.com>
2023-01-02 19:24:16 -0300

    feat: serializeTransactions

---
+++ src/api/serializeTransactions.ts
@@ -0,0 +1,41 @@
+import SerializedTransactions from '../types/SerializedTransactions';
+
+type Account = { user: { username: string } };
+
+type Transactions = {
+  createdAt: string;
+  creditedAccount: Account;
+  creditedAccountId: string;
+  debitedAccount: Account;
+  debitedAccountId: string;
+  id: string;
+  value: string;
+};
+
+const serializeTransactions = (
+  data: Transactions[],
+  accountId: string,
+): SerializedTransactions[] =>
+  data.map((transaction) => {
+    const {
+      createdAt,
+      creditedAccount,
+      creditedAccountId,
+      debitedAccount,
+      id,
+      value,
+    } = transaction;
+
+    return {
+      date: new Date(createdAt).toLocaleString(),
+      id,
+      username:
+        creditedAccountId === accountId
+          ? debitedAccount.user.username
+          : creditedAccount.user.username,
+      type: creditedAccountId === accountId ? 'cash-in' : 'cash-out',
+      value,
+    };
+  });
+
+export default serializeTransactions;
