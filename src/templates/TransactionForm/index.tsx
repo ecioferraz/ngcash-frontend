5ccde2bb546f26618e458628b8dbc240bbe04476
Écio Ferraz <ecioferraz@hotmail.com>
2023-01-03 12:58:42 -0300

    refactor: TransactionForm

--- src/templates/TransactionForm/index.tsx
+++ src/templates/TransactionForm/index.tsx
@@ -1,5 +1,5 @@
 import { isAxiosError } from 'axios';
-import { FormEvent, useState } from 'react';
+import { FormEvent, useEffect, useState } from 'react';
 import requestApi from '../../api/axios';
 import Button from '../../components/Button';
 import TextCard from '../../components/TextCard';
@@ -11,15 +11,21 @@ export default function TransactionForm() {
   const [transactionData, setTransactionData] = useState({
     loading: false,
     creditedUsername: '',
-    debitedUsername: readUser().username,
+    debitedUsername: '',
     value: '',
   });
   const [responseMessage, setResponseMessage] = useState('');
 
-  const { token } = readUser();
+  useEffect(() => {
+    setTransactionData((prev) => ({
+      ...prev,
+      debitedUsername: readUser().username,
+    }));
+  }, []);
 
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    const { token } = readUser();
 
     try {
       setTransactionData((prev) => ({ ...prev, loading: true }));
@@ -33,6 +39,11 @@ export default function TransactionForm() {
       });
 
       setResponseMessage('Transação concluída!');
+      setTransactionData((prev) => ({
+        ...prev,
+        creditedUsername: '',
+        value: '',
+      }));
     } catch (err) {
       if (isAxiosError(err)) setResponseMessage(err.response?.data.message);
     } finally {
@@ -47,19 +58,26 @@ export default function TransactionForm() {
           setTransactionData((prev) => ({ ...prev, creditedUsername: value }));
         }}
         placeholder="Transferir para..."
+        value={transactionData.creditedUsername}
       />
       <TextInput
         handleChange={({ target: { value } }) => {
-          setTransactionData((prev) => ({ ...prev, value }));
+          setTransactionData((prev) => ({
+            ...prev,
+            value: value.replace(',', '.'),
+          }));
         }}
         placeholder="Valor (R$)"
+        value={transactionData.value}
       />
       {responseMessage && (
         <TextCard as="p" size="xsmall">
           {responseMessage}
         </TextCard>
       )}
-      <Button type="submit">Realizar transferência</Button>
+      <Button disabled={+transactionData.value <= 0} type="submit">
+        Realizar transferência
+      </Button>
     </Styled.Container>
   );
 }
