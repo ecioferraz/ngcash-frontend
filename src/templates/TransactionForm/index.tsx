c72f6714d43bcec0cd9b6142f13ad8fa14e866b3
Écio Ferraz <ecioferraz@hotmail.com>
2023-01-04 20:19:44 -0300

    refactor: TransactionForm

--- src/templates/TransactionForm/index.tsx
+++ src/templates/TransactionForm/index.tsx
@@ -4,17 +4,19 @@ import requestApi from '../../api/axios';
 import Button from '../../components/Button';
 import TextCard from '../../components/TextCard';
 import TextInput from '../../components/TextInput';
+import useUsers from '../../hooks/useUsers';
 import { readUser } from '../../services/localStorage';
 import * as Styled from './styles';
 
 export default function TransactionForm() {
+  const [responseMessage, setResponseMessage] = useState('');
   const [transactionData, setTransactionData] = useState({
     loading: false,
     creditedUsername: '',
     debitedUsername: '',
     value: '',
   });
-  const [responseMessage, setResponseMessage] = useState('');
+  const { users } = useUsers();
 
   useEffect(() => {
     setTransactionData((prev) => ({
@@ -53,31 +55,40 @@ export default function TransactionForm() {
 
   return (
     <Styled.Container onSubmit={handleSubmit}>
-      <TextInput
-        handleChange={({ target: { value } }) => {
-          setTransactionData((prev) => ({ ...prev, creditedUsername: value }));
-        }}
-        placeholder="Transferir para..."
-        value={transactionData.creditedUsername}
-      />
-      <TextInput
-        handleChange={({ target: { value } }) => {
-          setTransactionData((prev) => ({
-            ...prev,
-            value: value.replace(',', '.'),
-          }));
-        }}
-        placeholder="Valor (R$)"
-        value={transactionData.value}
-      />
-      {responseMessage && (
-        <TextCard as="p" size="xsmall">
-          {responseMessage}
-        </TextCard>
-      )}
-      <Button disabled={+transactionData.value <= 0} type="submit">
-        Realizar transferência
-      </Button>
+      <section>
+        <TextInput
+          datalist={users}
+          handleChange={({ target: { value } }) => {
+            setTransactionData((prev) => ({
+              ...prev,
+              creditedUsername: value,
+            }));
+          }}
+          placeholder="Transferir para..."
+          value={transactionData.creditedUsername}
+        />
+        <TextInput
+          handleChange={({ target: { value } }) => {
+            setTransactionData((prev) => ({
+              ...prev,
+              value: value.replace(',', '.'),
+            }));
+          }}
+          placeholder="Valor (R$)"
+          value={transactionData.value}
+        />
+        {responseMessage && (
+          <TextCard as="p" size="xsmall">
+            {responseMessage}
+          </TextCard>
+          // conditional color if error or success
+        )}
+      </section>
+      <section>
+        <Button disabled={+transactionData.value <= 0} type="submit">
+          Realizar transferência
+        </Button>
+      </section>
     </Styled.Container>
   );
 }
