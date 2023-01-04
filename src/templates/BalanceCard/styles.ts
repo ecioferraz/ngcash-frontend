67c05c99386775a2ee13c320c47cb5d980a291b3
Écio Ferraz <ecioferraz@hotmail.com>
2023-01-04 14:30:49 -0300

    styles: BalanceCard

--- src/templates/BalanceCard/styles.ts
+++ src/templates/BalanceCard/styles.ts
@@ -1,5 +1,15 @@
 import styled, { css } from 'styled-components';
 
 export const Container = styled.section`
-  ${({ theme }) => css``}
+  ${({ theme }) => css`
+    border-radius: 15px;
+    background: ${theme.colors.white};
+    color: ${theme.colors.black};
+    padding: 0.5rem;
+    width: 80%;
+
+    > h6, p {
+      margin: 1rem;
+    }
+  `}
 `;
