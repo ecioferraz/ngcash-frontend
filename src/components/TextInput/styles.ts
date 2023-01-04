8f2984d0dbc1230f887a410b4db33107220b1d43
Écio Ferraz <ecioferraz@hotmail.com>
2023-01-04 20:17:49 -0300

    styles: TextInput

--- src/components/TextInput/styles.ts
+++ src/components/TextInput/styles.ts
@@ -1,5 +1,18 @@
 import styled, { css } from 'styled-components';
 
 export const Container = styled.input`
-  ${({ theme }) => css``}
+  ${({ theme }) => css`
+    background: transparent;
+    border: none;
+    border-bottom: solid 1px ${theme.colors.mediumGray};
+    color: ${theme.colors.white};
+    font-size: ${theme.font.sizes.small};
+    outline: none;
+
+    :focus {
+      border: none;
+      border-bottom: 1px solid ${theme.colors.lightGreen};
+      outline: none;
+    }
+  `}
 `;
