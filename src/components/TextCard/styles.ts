d4ec830fe8f36929fd127d82e7a39786fc567b50
Écio Ferraz <ecioferraz@hotmail.com>
2023-01-05 14:59:39 -0300

    styles: successful transaction message to green

--- src/components/TextCard/styles.ts
+++ src/components/TextCard/styles.ts
@@ -37,9 +37,16 @@ const mediaFont = (theme: DefaultTheme) => css`
   }
 `;
 
+const successTextColor = (message: string) =>
+  css`${({ theme }) =>
+    message.includes('Transação concluída') && {
+      color: theme.colors.lightGreen,
+    }}`;
+
 export const Container = styled.h1<Pick<TextCardProps, 'size' | 'uppercase'>>`
-  ${({ size = 'huge', theme, uppercase = false }) => css`
+  ${({ children, size = 'huge', theme, uppercase = false }) => css`
     ${textSize[size](theme)};
     ${textCase(uppercase)};
+    ${successTextColor(children as string)}
   `}
 `;
