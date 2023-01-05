551fb76c0ee8d947ef87d625dfb627d75f75f5ba
Écio Ferraz <ecioferraz@hotmail.com>
2023-01-05 15:34:14 -0300

    styles: TransactionsTable media

--- src/templates/TransactionsTable/styles.ts
+++ src/templates/TransactionsTable/styles.ts
@@ -1,12 +1,18 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 export const Container = styled.section`
+${({ theme }) => css`
   text-align: center;
   width: 100%;
 
   > h1 {
     margin-bottom: 0;
   }
+
+  @media ${theme.media.lteMedium} {
+    width: fit-content;
+  }
+`}
 `;
 
 export const Filters = styled.section`
