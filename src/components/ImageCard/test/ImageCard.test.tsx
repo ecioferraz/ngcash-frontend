68246978ebe930cbffa6e229d7bc585bf411f148
Écio Ferraz <ecioferraz@hotmail.com>
2023-01-09 15:33:29 -0300

    refactor: ImageCard test

--- src/components/ImageCard/test/ImageCard.test.tsx
+++ src/components/ImageCard/test/ImageCard.test.tsx
@@ -1,20 +1,21 @@
 import renderTheme from '../../../styles/renderTheme';
 import ImageCard from '..';
+import { screen } from '@testing-library/react';
 
 describe('<ImageCard />', () => {
   it('should render <ImageCard />', () => {
-    const { container } = renderTheme(
+    renderTheme(
       <ImageCard altText="test" src="../../assets/images/ngcash-logo.jpg" />,
     );
 
-    expect(container).toMatchInlineSnapshot(`
-      <div>
-        <img
-          alt="test"
-          class=""
-          src="../../assets/images/ngcash-logo.jpg"
-        />
-      </div>
+    const imageCard = screen.getByRole('img', { name: 'test' });
+
+    expect(imageCard).toMatchInlineSnapshot(`
+      <img
+        alt="test"
+        class=""
+        src="../../assets/images/ngcash-logo.jpg"
+      />
     `);
   });
 });
