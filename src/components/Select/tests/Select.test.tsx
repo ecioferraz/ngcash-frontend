199fc40e48b606f6c14da227851daf1232361ae5
Écio Ferraz <ecioferraz@hotmail.com>
2023-01-09 15:33:43 -0300

    refactor: Select test

--- src/components/Select/tests/Select.test.tsx
+++ src/components/Select/tests/Select.test.tsx
@@ -10,7 +10,49 @@ describe('<Select />', () => {
 
     const select = screen.getByRole('option', { name: 'Todas' }).parentElement;
 
-    expect(select).toBeInTheDocument();
     expect(select?.children.length).toBe(3);
+    expect(select).toMatchInlineSnapshot(`
+      .c0 {
+        background: transparent;
+        border: none;
+        border-bottom: solid 1px #DDDDDD;
+        color: #FFFFFF;
+        font-size: 1.6rem;
+        outline: none;
+        width: 10rem;
+      }
+
+      .c0:focus {
+        border: none;
+        border-bottom: 1px solid #00D775;
+        outline: none;
+      }
+
+      .c0 > option {
+        background: #000000;
+        font-size: 1.6rem;
+        height: 0.8rem;
+      }
+
+      <select
+        class="c0"
+      >
+        <option
+          value="all"
+        >
+          Todas
+        </option>
+        <option
+          value="cash-in"
+        >
+          Entradas
+        </option>
+        <option
+          value="cash-out"
+        >
+          Saídas
+        </option>
+      </select>
+    `);
   });
 });
