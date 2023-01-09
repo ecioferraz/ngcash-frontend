eb1c7dd188b42af2d5e9fbf975d3d32d3c93637e
Écio Ferraz <ecioferraz@hotmail.com>
2023-01-09 15:34:29 -0300

    refactor: TextInput test

--- src/components/TextInput/tests/TextInput.test.tsx
+++ src/components/TextInput/tests/TextInput.test.tsx
@@ -6,10 +6,42 @@ describe('<TextInput />', () => {
   const onChange = jest.fn();
 
   it('should render <TextInput />', () => {
-    renderTheme(<TextInput handleChange={onChange} placeholder="TextInput" />);
+    renderTheme(
+      <TextInput
+        datalist={['user1', 'user2']}
+        handleChange={onChange}
+        minLength={8}
+        placeholder="TextInput"
+        value=""
+      />,
+    );
 
     const textInput = screen.getByPlaceholderText('TextInput');
 
-    expect(textInput).toBeInTheDocument();
+    expect(textInput).toMatchInlineSnapshot(`
+      .c0 {
+        background: transparent;
+        border: none;
+        border-bottom: solid 1px #DDDDDD;
+        color: #FFFFFF;
+        font-size: 1.6rem;
+        outline: none;
+      }
+
+      .c0:focus {
+        border: none;
+        border-bottom: 1px solid #00D775;
+        outline: none;
+      }
+
+      <input
+        class="c0"
+        list="usernames"
+        minlength="8"
+        placeholder="TextInput"
+        type="text"
+        value=""
+      />
+    `);
   });
 });
