5269ac708ca12a18cae056ec4545ca8ff77750bd
Écio Ferraz <ecioferraz@hotmail.com>
2023-01-12 13:23:13 -0300

    refactor: remove unnecessary Header stories config

--- src/templates/Header/stories.tsx
+++ src/templates/Header/stories.tsx
@@ -1,10 +1,7 @@
 import { Meta, Story } from '@storybook/react/types-6-0';
-// import HeaderProps from './HeaderProps';
 import Header from '.';
 
 export default {
-  args: { children: 'Header' },
-  argTypes: { children: { type: 'string' } },
   component: Header,
   title: 'Templates/Header',
 } as Meta;
